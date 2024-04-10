01bf4a34138ba551eaf0add3a1218d59ac34f222
Praveen <praveenbuddies@gmail.com>
2024-04-10 19:29:46 +0100

    java 17

---
+++ src/main/java/com/pravinmantena/cjadvanced/corejava17/Corejava17Application.java
@@ -0,0 +1,13 @@
+package com.pravinmantena.cjadvanced.corejava17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Corejava17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Corejava17Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pravinmantena/cjadvanced/corejava17/Corejava17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pravinmantena.cjadvanced.corejava17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Corejava17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
